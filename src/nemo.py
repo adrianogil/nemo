78ebdc1ce265da1490971988626cd52c18ead0bd
Adriano Gil <adrianogil.san@gmail.com>
2018-03-24 15:09:33 -0400

    List cs scripts and shaders

--- src/nemo.py
+++ src/nemo.py
@@ -4,6 +4,8 @@ from os.path import join
 
 import sys
 
+import subprocess
+
 search_path = sys.argv[1]
 
 assets_file_path = []
@@ -24,5 +26,20 @@ for f in assets_file_path:
             u_paths.append(f)
             break
 
+print('Found %s Unity projects\n' % (len(u_paths)))
+
 for u in u_paths:
-    print(u)
+    project_path = u
+    project_name = os.path.basename(project_path)
+    print('Project ' + project_name)
+    print('  Path ' + project_path)
+
+    cs_number_cmd = 'find "' + project_path + '" -name "*.cs" | wc -l'
+    cs_number_output = subprocess.check_output(cs_number_cmd, shell=True)
+    
+    print('  Total CS scripts: ' + cs_number_output.strip())
+
+    shader_number_cmd = 'find "' + project_path + '" -name "*.shader" | wc -l'
+    shader_number_output = subprocess.check_output(cs_number_cmd, shell=True)
+
+    print('  Total shader scripts: ' + shader_number_output.strip())
