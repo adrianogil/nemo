2ba3cd9512914a7a26be879ec2cf350b1394eaec
Adriano Gil <adrianogil.san@gmail.com>
2018-03-26 11:07:32 -0400

    Added bashrc

---
+++ src/bashrc.sh
@@ -0,0 +1,6 @@
+# Add the following lines to bashrc.sh/profile.sh:
+# NEMO - UnityProject Finder
+# export NEMO_DIR=/Users/SIDIA/workspace/python/moi/nemo/
+# source $NEMO_DIR/src/bashrc.sh
+
+alias nemo="python2 $NEMO_DIR/src/nemo.py"
