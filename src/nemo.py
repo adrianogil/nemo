3eb99b9551fa103c6c10f518fee050a59152eb0d
Adriano Gil <adrianogil.san@gmail.com>
2020-08-02 09:55:45 -0400

    Added shader flag

--- src/nemo.py
+++ src/nemo.py
@@ -1,3 +1,5 @@
+from pyutils.cli.flags import verify_flag
+
 import os
 import fnmatch
 from os.path import join
@@ -6,54 +8,69 @@ import sys
 
 import subprocess
 
-search_path = sys.argv[1]
 
-assets_file_path = []
-psettings_file_path = []
+def search_projects():
+    search_path = sys.argv[1]
 
-for root, subFolders, files in os.walk(search_path):
-    for folder in subFolders:
-        if folder == 'Assets':
-            assets_file_path.append(root)
-        if folder == "ProjectSettings":
-            psettings_file_path.append(root)
+    assets_file_path = []
+    psettings_file_path = []
 
-u_paths = []
+    for root, subFolders, files in os.walk(search_path):
+        for folder in subFolders:
+            if folder == 'Assets':
+                assets_file_path.append(root)
+            if folder == "ProjectSettings":
+                psettings_file_path.append(root)
 
-for f in assets_file_path:
-    for f2 in psettings_file_path:
-        if f == f2:
-            u_paths.append(f)
-            break
+    u_paths = []
 
-print('Found %s Unity projects\n' % (len(u_paths)))
+    for f in assets_file_path:
+        for f2 in psettings_file_path:
+            if f == f2:
+                u_paths.append(f)
+                break
 
-for u in u_paths:
-    project_path = u
-    project_name = os.path.basename(project_path)
-    print('Project ' + project_name)
-    print('  Path ' + project_path)
+    print('Found %s Unity projects\n' % (len(u_paths)))
 
-    project_path = os.path.join(project_path, "Assets/")
+    shader_flag = verify_flag("--shader")
+    csharp_flag = verify_flag("--csharp")
+    no_flag = not csharp_flag and not shader_flag
 
-    cs_number_cmd = 'find "' + project_path + '" -name "*.cs" | wc -l'
-    cs_number_output = subprocess.check_output(cs_number_cmd, shell=True)
-    cs_number_output = cs_number_output.decode("utf8")
+    show_cs_files = csharp_flag or no_flag
+    show_shader_files = shader_flag or no_flag
 
-    cs_lines_cmd = '(find "' + project_path + '" -name "*.cs" | xargs -I {} cat {}) | wc -l'
-    cs_lines_output = subprocess.check_output(cs_lines_cmd, shell=True)
-    cs_lines_output = cs_lines_output.decode("utf8")
+    for u in u_paths:
+        project_path = u
+        project_name = os.path.basename(project_path)
+        print('Project ' + project_name)
+        print('  Path ' + project_path)
 
-    print('  Total CS scripts: ' + cs_number_output.strip() + ' ( LOC: ' + \
-        cs_lines_output.strip() + ')')
+        project_path = os.path.join(project_path, "Assets/")
 
-    shader_number_cmd = 'find "' + project_path + '" -name "*.shader" | wc -l'
-    shader_number_output = subprocess.check_output(shader_number_cmd, shell=True)
-    shader_number_output = shader_number_output.decode("utf8")
+        if show_cs_files:
+            cs_number_cmd = 'find "' + project_path + '" -name "*.cs" | wc -l'
+            cs_number_output = subprocess.check_output(cs_number_cmd, shell=True)
+            cs_number_output = cs_number_output.decode("utf8")
 
-    shader_lines_cmd = '(find "' + project_path + '" -name "*.shader" | xargs -I {} cat {}) | wc -l'
-    shader_lines_output = subprocess.check_output(shader_lines_cmd, shell=True)
-    shader_lines_output = shader_lines_output.decode("utf8")
+            cs_lines_cmd = '(find "' + project_path + '" -name "*.cs" | xargs -I {} cat {}) | wc -l'
+            cs_lines_output = subprocess.check_output(cs_lines_cmd, shell=True)
+            cs_lines_output = cs_lines_output.decode("utf8")
 
-    print('  Total shader scripts: ' + shader_number_output.strip() + ' ( LOC: ' + \
-        shader_lines_output.strip() + ')')
+            print('  Total CS scripts: ' + cs_number_output.strip() + ' ( LOC: ' + \
+                cs_lines_output.strip() + ')')
+
+        if show_shader_files:
+            shader_number_cmd = 'find "' + project_path + '" -name "*.shader" | wc -l'
+            shader_number_output = subprocess.check_output(shader_number_cmd, shell=True)
+            shader_number_output = shader_number_output.decode("utf8")
+
+            shader_lines_cmd = '(find "' + project_path + '" -name "*.shader" | xargs -I {} cat {}) | wc -l'
+            shader_lines_output = subprocess.check_output(shader_lines_cmd, shell=True)
+            shader_lines_output = shader_lines_output.decode("utf8")
+
+            print('  Total shader scripts: ' + shader_number_output.strip() + ' ( LOC: ' + \
+                shader_lines_output.strip() + ')')
+
+
+if __name__ == '__main__':
+    search_projects()
